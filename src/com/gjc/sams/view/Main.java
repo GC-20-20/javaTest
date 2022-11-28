8b9c15fc5627c0137783fe7313556bb26984f989
GC2020 <1519203603@qq.com>
2022-11-28 17:33:57 +0800

    默认的

--- src/com/gjc/sams/view/Main.java
+++ src/com/gjc/sams/view/Main.java
@@ -19,6 +19,7 @@ public class Main {
             }
         }catch(Exception e) {
             System.out.println(e);
+            System.out.println("hellowrld");
         }
         new View1();
     }
